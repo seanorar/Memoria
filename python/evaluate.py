77aaf64b87206d4f7810f9484dd356ed0562bedc
seanorar <seba_orme@hotmail.com>
2017-10-11 11:03:30 -0300

    funcion para cargar y graficar datos

--- python/evaluate.py
+++ python/evaluate.py
@@ -112,12 +112,14 @@ def data_to_graphs(txt_data, path_imgs, path_xmls, prototxt, caffemodel, mode="c
     np.savez(output + "_presicion", r_presicion)
     np.savez(output + "_recall", r_recall)
 
-def to_plot():
-    r_iou = [0.44584649527506504, 0.5062535687118969, 0.5342011713109763, 0.5532779513454696, 0.5690704146371486, 0.5838816962721233, 0.598112450293735, 0.6119567583581103, 0.6259686894492862, 0.6397117606770278, 0.6559784835854291, 0.6708644089387157, 0.6884734388748447, 0.7091674284905564, 0.749944744786584, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933]
-    r_presicion = [0.27912128143837894, 0.1576551129848432, 0.13440007231696488, 0.11668162937366347, 0.10163280880613884, 0.0889425862375645, 0.07977745336089577, 0.07446203093336243, 0.0720217676800067, 0.07375549934508299, 0.07758539567458296, 0.0830987734610237, 0.08955421848038438, 0.10010830165600668, 0.12440397421323002, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856]
-    r_recall = [0.6675047257722235, 0.7304993383984629, 0.7654896926297471, 0.7965042258040684, 0.8318227465598921, 0.8747422384753438, 0.9162677577373299, 0.9466677801628421, 0.9700637983480157, 0.9840750082912066, 0.9906483323571336, 0.9925456660733788, 0.99410473916508, 0.9948819852676105, 0.9956407834770172, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861]
-
-
+def to_plot(dataset):
+    #r_iou = [0.44584649527506504, 0.5062535687118969, 0.5342011713109763, 0.5532779513454696, 0.5690704146371486, 0.5838816962721233, 0.598112450293735, 0.6119567583581103, 0.6259686894492862, 0.6397117606770278, 0.6559784835854291, 0.6708644089387157, 0.6884734388748447, 0.7091674284905564, 0.749944744786584, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933]
+    #r_presicion = [0.27912128143837894, 0.1576551129848432, 0.13440007231696488, 0.11668162937366347, 0.10163280880613884, 0.0889425862375645, 0.07977745336089577, 0.07446203093336243, 0.0720217676800067, 0.07375549934508299, 0.07758539567458296, 0.0830987734610237, 0.08955421848038438, 0.10010830165600668, 0.12440397421323002, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856]
+    #r_recall = [0.6675047257722235, 0.7304993383984629, 0.7654896926297471, 0.7965042258040684, 0.8318227465598921, 0.8747422384753438, 0.9162677577373299, 0.9466677801628421, 0.9700637983480157, 0.9840750082912066, 0.9906483323571336, 0.9925456660733788, 0.99410473916508, 0.9948819852676105, 0.9956407834770172, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861]
+    path = "/home/sebastian/Escritorio/universidad/memoria/py-faster-rcnn/tools/Memoria/calculados"
+    r_iou = np.load(path+"/"+dataset+"_1_iou.npz")['arr_0']
+    r_presicion = np.load(path + "/"+dataset+"_1_precision.npz")['arr_0']
+    r_recall = np.load(path + "/"+dataset+"_1_recall.npz")['arr_0']
     list_touple=[]
     for i in range(0, len(r_presicion)):
         list_touple.append((r_presicion[i], r_recall[i]))
@@ -134,6 +136,47 @@ def to_plot():
     plt.xlabel('recall')
     plt.show()
 
+def plot_both():
+    #r_iou = [0.44584649527506504, 0.5062535687118969, 0.5342011713109763, 0.5532779513454696, 0.5690704146371486, 0.5838816962721233, 0.598112450293735, 0.6119567583581103, 0.6259686894492862, 0.6397117606770278, 0.6559784835854291, 0.6708644089387157, 0.6884734388748447, 0.7091674284905564, 0.749944744786584, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933, 0.7509030749337933]
+    #r_presicion = [0.27912128143837894, 0.1576551129848432, 0.13440007231696488, 0.11668162937366347, 0.10163280880613884, 0.0889425862375645, 0.07977745336089577, 0.07446203093336243, 0.0720217676800067, 0.07375549934508299, 0.07758539567458296, 0.0830987734610237, 0.08955421848038438, 0.10010830165600668, 0.12440397421323002, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856, 0.12492992890257856]
+    #r_recall = [0.6675047257722235, 0.7304993383984629, 0.7654896926297471, 0.7965042258040684, 0.8318227465598921, 0.8747422384753438, 0.9162677577373299, 0.9466677801628421, 0.9700637983480157, 0.9840750082912066, 0.9906483323571336, 0.9925456660733788, 0.99410473916508, 0.9948819852676105, 0.9956407834770172, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861, 0.9956906737184861]
+    path = "/home/sebastian/Escritorio/universidad/memoria/py-faster-rcnn/tools/Memoria/calculados"
+    dataset = "imagenet"
+    r_iou = np.load(path+"/"+dataset+"_1_iou.npz")['arr_0']
+    r_presicion = np.load(path + "/"+dataset+"_1_precision.npz")['arr_0']
+    r_recall = np.load(path + "/"+dataset+"_1_recall.npz")['arr_0']
+    list_touple=[]
+    for i in range(0, len(r_presicion)):
+        list_touple.append((r_presicion[i], r_recall[i]))
+
+    sorted_by_second = sorted(list_touple, key=lambda tup: tup[1])
+    n_p_i = []
+    n_r_i = []
+    for i in range(0, len(r_presicion)):
+        n_p_i.append(sorted_by_second[i][0])
+        n_r_i.append(sorted_by_second[i][1])
+
+    dataset = "pascal"
+    r_iou = np.load(path+"/"+dataset+"_1_iou.npz")['arr_0']
+    r_presicion = np.load(path + "/"+dataset+"_1_precision.npz")['arr_0']
+    r_recall = np.load(path + "/"+dataset+"_1_recall.npz")['arr_0']
+    list_touple=[]
+    for i in range(0, len(r_presicion)):
+        list_touple.append((r_presicion[i], r_recall[i]))
+
+    sorted_by_second = sorted(list_touple, key=lambda tup: tup[1])
+    n_p_p = []
+    n_r_p = []
+    for i in range(0, len(r_presicion)):
+        n_p_p.append(sorted_by_second[i][0])
+        n_r_p.append(sorted_by_second[i][1])
+
+    plt.plot(n_r_i, n_p_i, label="imagenet")
+    plt.plot(n_r_p, n_p_p, label = "pascal" )
+    plt.ylabel('precision')
+    plt.xlabel('recall')
+    plt.legend()
+    plt.show()
 
 #imagenet
 #-------------------------------------------------------------------------
@@ -150,17 +193,18 @@ def to_plot():
 
 #pascal
 #---------------------------------------------------------------------------
-txt_data = "/home/sormeno/Datasets/Pascal/val.txt"
-path_imgs = "/home/sormeno/Datasets/Pascal/Images/"
-path_xmls = "/home/sormeno/Datasets/Pascal/xmls/"
+#txt_data = "/home/sormeno/Datasets/Pascal/val.txt"
+#path_imgs = "/home/sormeno/Datasets/Pascal/Images/"
+#path_xmls = "/home/sormeno/Datasets/Pascal/xmls/"
 
-prototxt =  "/home/sormeno/py-faster-rcnn/models/pascal_voc/VGG16/faster_rcnn_end2end/test.prototxt"
-caffemodel = "/home/sormeno/py-faster-rcnn/data/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel"
+#prototxt =  "/home/sormeno/py-faster-rcnn/models/pascal_voc/VGG16/faster_rcnn_end2end/test.prototxt"
+#caffemodel = "/home/sormeno/py-faster-rcnn/data/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel"
 #----------------------------------------------------------------------------
-data_to_graphs(txt_data, path_imgs, path_xmls, prototxt, caffemodel, "gpu", "/home/sormeno/pascal_1")
+#data_to_graphs(txt_data, path_imgs, path_xmls, prototxt, caffemodel, "gpu", "/home/sormeno/pascal_1")
 
-prototxt =  "/home/sormeno/py-faster-rcnn/models/imagenet/VGG16/faster_rcnn_end2end/test.prototxt"
-caffemodel = "/home/sormeno/py-faster-rcnn/data/faster_rcnn_models/VGG16_faster_rcnn_imagenet.caffemodel"
+#prototxt =  "/home/sormeno/py-faster-rcnn/models/imagenet/VGG16/faster_rcnn_end2end/test.prototxt"
+#caffemodel = "/home/sormeno/py-faster-rcnn/data/faster_rcnn_models/VGG16_faster_rcnn_imagenet.caffemodel"
 
-data_to_graphs(txt_data, path_imgs, path_xmls, prototxt, caffemodel, "gpu", "/home/sormeno/imagenet_1")
+#data_to_graphs(txt_data, path_imgs, path_xmls, prototxt, caffemodel, "gpu", "/home/sormeno/imagenet_1")
 
+plot_both()
