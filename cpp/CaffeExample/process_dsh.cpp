19966363b29a548095ecb34c8d3a89bdaa41f70a
Sebastian <seba_orme@hotmail.com>
2017-10-30 16:53:14 -0300

    codigo para extraccion de descriptores DSH

--- cpp/CaffeExample/process_dsh.cpp
+++ cpp/CaffeExample/process_dsh.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 void get_features(string imgs_path_folder, string imgs_txt, string data_out){
         //extraccion de features a partir de los bbox
-        string str_pt("/home/sormeno/Models/DSH/dsh.prototxt");
-        string str_caffemodel("/home/sormeno/Models/DSH/dsh.caffemodel");
+        string str_pt("/home/sormeno/Models/DSH/dsh_ft.prototxt");
+        string str_caffemodel("/home/sormeno/Models/DSH/dsh_ft.caffemodel");
         CaffePredictor caffe_predictor(str_pt, str_caffemodel, 256, 256, CAFFE_GPU_MODE);
 
 	ofstream writeFile (data_out, ios::out | ios::binary);
@@ -20,7 +20,7 @@ void get_features(string imgs_path_folder, string imgs_txt, string data_out){
 		cout << str << endl;
         	int des_size = 0;
         	cv::Mat img = cv ::imread(imgs_path_folder + str);
-        	float * des_im = caffe_predictor.getCaffeDescriptor(img, &des_size, "ip1");
+        	float * des_im = caffe_predictor.getCaffeDescriptor(img, &des_size, "ip1_f");
 		writeFile.write((char*) des_im, sizeof(float) * des_size);
 	}
 	writeFile.close();
@@ -29,6 +29,6 @@ void get_features(string imgs_path_folder, string imgs_txt, string data_out){
 int main(int argc, char* argv[]){
 	string imgs_path_folder = "/home/sormeno/Datasets/VOC/val/val/";
 	string imgs_txt = "/home/sormeno/Datasets/VOC/val/val.txt";
-	string data_out = "/home/sormeno/dsh_result.bin";
+	string data_out = "/home/sormeno/dsh_ft_result.bin";
 	get_features(imgs_path_folder, imgs_txt, data_out);
 }
